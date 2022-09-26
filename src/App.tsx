d097fc916bb5479285c60f77a150d8336423bc67
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-27 07:29:00 +0900

    feat: show only target section when media print

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Report from './page/Report';
+import UserInterface from './page/UserInterface';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function onPrint() {
+		window.print();
+	}
+	return (
+		<>
+			<DoNotPrint>
+				<UserInterface onPrint={onPrint} />
+			</DoNotPrint>
+			<DoPrint>
+				<Report />
+			</DoPrint>
+		</>
+	);
 }
+
+const DoNotPrint = styled.div`
+	@media print {
+		display: none;
+	}
+`;
+
+const DoPrint = styled.div`
+	display: none;
+
+	@media print {
+		display: block;
+	}
+`;
 
 export default App;
