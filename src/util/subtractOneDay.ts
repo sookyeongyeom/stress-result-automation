06eee8a93646b42127c4209e9bcbd75a5d6dcaeb
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-03 20:33:09 +0900

    fix: subtractOneDay error handling

--- src/util/subtractOneDay.ts
+++ src/util/subtractOneDay.ts
@@ -1,5 +1,9 @@
 export default function subtractOneDay(date: string) {
-	const splited = date.split('-');
-	splited[2] = (Number(date.split('-')[2]) - 1).toString().padStart(2, '0');
-	return splited.join('-');
+	try {
+		const splited = date.split('-');
+		splited[2] = (Number(date.split('-')[2]) - 1).toString().padStart(2, '0');
+		return splited.join('-');
+	} catch {
+		return 'Invalid Date';
+	}
 }
